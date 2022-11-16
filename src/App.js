ca2c4eb9dd5246e0d2f43f98cbae06972ba4a327
SHADOW-LUCKY <67065014+SHADOW-LUCKY@users.noreply.github.com>
2022-11-16 07:31:26 -0500

    1.1
    fix error call http

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   const consultarAPI = async () => {
    if (consultar) {
     const appId = 'bb1a5824e181e81f687abe457e36032e'
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`
 
     const respuesta = await fetch(url)
     const resultado = await respuesta.json()
